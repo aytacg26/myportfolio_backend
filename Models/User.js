9dc2e1a4b730e16726676884eac4f8db3212f3d1
DEO <DEO@DESKTOP-1AAKV4M>
2021-02-17 16:32:37 +0200

    User Model Created

--- Models/User.js
+++ Models/User.js
@@ -1,1 +1,43 @@
 import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    requried: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    //when user confirms his/her email this will be true, if user deactives his/her accout, this will be false
+    type: Boolean,
+    default: false,
+  },
+  isConfirmed: {
+    //When user confirms his/her email this will be true
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const User = mongoose.model('users', UserSchema);
+
+export default User;
